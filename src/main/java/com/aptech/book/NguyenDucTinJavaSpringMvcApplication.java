24210e07cff6eb9275248793271ebc6d2f4136de
TIN-THIET-KE <TIN-THIET-KE@TIN-THIET-KE>
2021-11-07 08:48:15 +0700

    Initial commit

---
+++ src/main/java/com/aptech/book/NguyenDucTinJavaSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.aptech.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenDucTinJavaSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NguyenDucTinJavaSpringMvcApplication.class, args);
+    }
+
+}
