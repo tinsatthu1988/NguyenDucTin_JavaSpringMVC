c28baf0d274df701d8db585638354dacf106ea67
TIN-THIET-KE <TIN-THIET-KE@TIN-THIET-KE>
2021-11-11 10:17:53 +0700

    search user

--- src/main/java/com/aptech/book/controller/UserController.java
+++ src/main/java/com/aptech/book/controller/UserController.java
@@ -29,15 +29,17 @@ public class UserController {
 
     @GetMapping("/users")
     public String listFirstPage(Model model) {
-        return listByPage(1, model, "firstName", "asc");
+        return listByPage(1, model, "firstName", "asc", null);
     }
 
     @GetMapping("/users/page/{pageNum}")
-    public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model,
-                             @Param("sortField") String sortField, @Param("sortDir") String sortDir
+    public String listByPage(
+            @PathVariable(name="pageNum") int pageNum, Model model,
+            @Param("sortField") String sortField, @Param("sortDir") String sortDir,
+            @Param("keyword") String keyword
     ) {
 
-        Page<User> page = service.listByPage(pageNum, sortField, sortDir);
+        Page<User> page = service.listByPage(pageNum, sortField, sortDir, keyword);
         List<User> listUsers = page.getContent();
 
         long startCount = (pageNum - 1) * UserService.USERS_PER_PAGE + 1;
@@ -57,6 +59,7 @@ public class UserController {
         model.addAttribute("sortField", sortField);
         model.addAttribute("sortDir", sortDir);
         model.addAttribute("reverseSortDir", reverseSortDir);
+        model.addAttribute("keyword", keyword);
 
         return "users/users";
     }
