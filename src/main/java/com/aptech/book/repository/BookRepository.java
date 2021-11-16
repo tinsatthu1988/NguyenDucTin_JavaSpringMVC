d7ffac68b9a5fd9a8fe8f188e250feb7542d44f6
tin <tinsatthu1988@gmail.com>
2021-11-16 22:26:17 +0700

    add book

--- src/main/java/com/aptech/book/repository/BookRepository.java
+++ src/main/java/com/aptech/book/repository/BookRepository.java
@@ -9,6 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 public interface BookRepository extends JpaRepository<Book, Integer> {
     public Book findByTitle(String title);
 
-    @Query("SELECT b FROM Book b WHERE CONCAT(b.id , ' ', b.title, ' ', b.author, ' ', b.description, ' ', b.category.name) LIKE %?1%")
+    @Query("SELECT b FROM Book b WHERE CONCAT(b.id , ' ', b.title, ' ', b.author, ' ', b.description, ' ', b.price, ' ', b.category.name) LIKE %?1%")
     public Page<Book> findAll(String keyword, Pageable pageable);
 }
