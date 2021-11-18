80b7559e432a2b06eaf99e4f3f6cde4cf3a77ba1
TIN-THIET-KE <TIN-THIET-KE@TIN-THIET-KE>
2021-11-18 11:36:31 +0700

    list book client

--- src/main/java/com/aptech/book/MainController.java
+++ src/main/java/com/aptech/book/MainController.java
@@ -1,15 +1,32 @@
 package com.aptech.book;
 
+import com.aptech.book.entity.Book;
+import com.aptech.book.service.BookService;
+import com.aptech.book.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 class MainController {
+
+    @Autowired
+    private BookService bookService;
+
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping("")
-    public String viewHomePage() {
+    public String viewHomePage(Model model) {
+        List<Book> listBooks = bookService.listAll();
+        model.addAttribute("listBooks", listBooks);
+
         return "home";
     }
 
